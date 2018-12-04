552686b795d91ce96b074696ae2b80e5b88a9501
dominich2 <domheng66@gmail.com>
2018-12-04 22:11:27 +0800

    updates styles of homepage

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { Avatar } from 'react-native-elements';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Text>staph</Text>
+      <Avatar
+      size="large"
+      source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg"}}
+      onPress={() => console.log("Works!")}
+      activeOpacity={0.7}
+      />
+      <Button 
+      title={"stop"} 
+      onPress={()=> {console.log('ok'); }}
+      color="black">
+      </Button>
       </View>
     );
   }
@@ -16,6 +29,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
 });
